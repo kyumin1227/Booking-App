2cce0c2c6e8edc5d3c37f42b62aad0701663bb8c
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-09-13 11:50:35 +0900

    Error fix & code change

--- Booking-App/src/App.tsx
+++ Booking-App/src/App.tsx
@@ -23,14 +23,14 @@ function App() {
         {/* Navbar가 필요한 route */}
         <Route path='/' element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path='calendar' element={<CalendarPage />} />
+          <Route path='/calendar' element={<CalendarPage />} />
         </Route>
 
-        {/* Navbar가 필요한 route */}
+        {/* Navbar가 필요없는 route */}
         <Route path='/noNav' element={<HomePage />} />
       </Routes>
     </>
   )
 }
 
-export default App
+export default App;
--- Booking-App/src/client/pages/CalendarPage/DateSeparate.tsx
+++ Booking-App/src/client/pages/CalendarPage/DateSeparate.tsx
@@ -4,21 +4,24 @@ import { dateString } from '../../atoms';
 function DateSeparate() {
 
     const date = useRecoilValue(dateString);
-    console.log(date);
-    
+
+    const year = date.slice(10, 15);
+    const month = date.slice(4, 7);
+    const day = date.slice(8, 10);
+    const week = date.slice(0, 3);
     
     return (
         <>
-            <h1>date</h1>
-            <span>{date ? date.toString() : null}</span>
+            <h1>Date</h1>
+            <span>{date ? date : null}</span>
             <br />
-            <span>년: { date ? (date.toString()).slice(10, 15) : null }</span>
+            <span>년: { date ? year : null }</span>
             <br />
-            <span>월: { date ? (date.toString()).slice(4, 7) : null }</span>
+            <span>월: { date ? month : null }</span>
             <br />
-            <span>일: { date ? (date.toString()).slice(8, 10) : null }</span>
+            <span>일: { date ? day : null }</span>
             <br />
-            <span>요일: { date ? (date.toString()).slice(0, 3) : null }</span>
+            <span>요일: { date ? week : null }</span>
         </>
     )
 }
