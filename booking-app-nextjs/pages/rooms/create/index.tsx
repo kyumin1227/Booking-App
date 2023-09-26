6e06843371db602d95304ec8b567625c74ea12b1
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-09-26 17:06:17 +0900

    방 생성의 전달 데이터 변경 (더미 데이터 -> 입력 데이터)

--- booking-app-nextjs/pages/rooms/create/index.tsx
+++ booking-app-nextjs/pages/rooms/create/index.tsx
@@ -2,37 +2,38 @@ import React, { HtmlHTMLAttributes, useEffect, useState } from "react"
 import axios from "@/lib/axios";
 
 export default function CreateRoom() {
-    const [startTime, setStartTime] = useState("");
 
-    const handleStartTime = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target.value);
-        setStartTime(e.target.value)
-    }
+    const [roomName, setRoomName] = useState('');
+    const [startTime, setStartTime] = useState('');
+    const [endTime, setEndTime] = useState('');
+    const [createDate, setCreateDate] = useState('');
+    const [timeSeparate, setTimeSeparate] = useState('');
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.defaultPrevented
+        e.preventDefault();
+        
         axios.post("/rooms", {
-            "roomName": " badRoom",
-            "startTime": 8,
-            "endTime": 20.5,
-            "createDate": 30,
-            "timeUnit": 60,
+            "roomName": roomName,
+            "startTime": startTime, // ex "13:30"
+            "endTime": endTime,
+            "createDate": createDate,
+            "timeUnit": timeSeparate,
         })
     } 
 
     return (<>
         <h1>방 생성</h1>
         <form onSubmit={handleSubmit} >
-            <label htmlFor="roomName" >방 이름 </label>
-            <input id="roomName" placeholder="방 이름" /><br/>
-            <label htmlFor="startTime" >운영시간 </label>
-            <input id="startTime" placeholder="" type="time" value={startTime} onChange={handleStartTime} />
+            <label htmlFor="roomName">방 이름</label>
+            <input id="roomName" placeholder="방 이름" value={roomName} onChange={(e) => setRoomName(e.target.value)} /><br />
+            <label htmlFor="startTime">운영시간</label>
+            <input id="startTime" placeholder="" type="time" value={startTime} onChange={(e) => setStartTime(e.target.value)} />
             <span> ~ </span>
-            <input id="endTime" placeholder="" type="time" min={startTime} /><br />
-            <label htmlFor="createDate" >미리 생성할 날짜 </label>
-            <input id="createDate" type="number" min="30" max="100" /><br />
-            <label htmlFor="timeSeparate" >시간 단위(분) </label>
-            <input id="timeSeparate" type="number" min="30" max="60" step="30" /><br />
+            <input id="endTime" placeholder="" type="time" min={startTime} value={endTime} onChange={(e) => setEndTime(e.target.value)} /><br />
+            <label htmlFor="createDate">미리 생성할 날짜</label>
+            <input id="createDate" type="number" min="30" max="100" value={createDate} onChange={(e) => setCreateDate(e.target.value)} /><br />
+            <label htmlFor="timeSeparate">시간 단위(분)</label>
+            <input id="timeSeparate" type="number" min="30" max="60" step="30" value={timeSeparate} onChange={(e) => setTimeSeparate(e.target.value)} /><br />
             <input placeholder="" type="submit" />
         </form>
     </>)
