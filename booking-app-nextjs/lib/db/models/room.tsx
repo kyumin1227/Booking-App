bdda26c33cd24c841d5eb5b679ecd99a6d21ab4f
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-09-28 18:22:42 +0900

    모델 수정 후 재생성 - startTime과 endTime의 타입을 Date에서 String으로 변경

--- booking-app-nextjs/lib/db/models/room.tsx
+++ booking-app-nextjs/lib/db/models/room.tsx
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { deleteModel } from "mongoose";
 
 const roomSchema = new mongoose.Schema(
     {   
@@ -6,18 +6,22 @@ const roomSchema = new mongoose.Schema(
         info: { type: String, default: "" },
         price: { type: Number, default: 0 },
         timeUnit: { type: Number, default: 60 }, // 예약 시간 단위
-        startTime: { type: Date, default: Date.now },
-        endTime: { type: Date, default: Date.now },
+        startTime: { type: String, default: "00:00" },
+        endTime: { type: String, default: "24:00" },
         test: {type: String, default: "안녕"},
         booked: [{ type: mongoose.Schema.Types.ObjectId, ref: "reservations" }],
         createAt: {type: Date, default: Date.now}
     }
 )
 
+    // 모델 수정 시 기존 모델 삭제
+    // mongoose.deleteModel("Rooms")
+
+
 const RoomModel =
-//   mongoose.models.Rooms ||
+  mongoose.models.Rooms ||
     mongoose.model("Rooms", roomSchema);
-  
+
 export default RoomModel;
 
 // 이름, 정보, 예약 현황, 휴일, 사용 가능 인원수, 최소 예약 시간, 가격, 소유자(관리자), 주소
