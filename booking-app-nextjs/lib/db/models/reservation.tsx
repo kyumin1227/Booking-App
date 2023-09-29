fc5f5437d33252bf2d2fb412a9b3a7e23407ab44
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-09-29 18:09:07 +0900

    예약 모델의 데이터 타입 변경

--- booking-app-nextjs/lib/db/models/reservation.tsx
+++ booking-app-nextjs/lib/db/models/reservation.tsx
@@ -6,12 +6,16 @@ const reservationSchema = new mongoose.Schema(
         reservations: [{
                 room: { type: mongoose.Schema.Types.ObjectId, require },
                 // user: {type: mongoose.Schema.Types.ObjectId, require},
-                time: {type: Number},
+                startTime: { type: Date },
+                endTime: { type: Date },
                 createAt: {type: Date, default: Date.now}
             }]
         
     }
 )
+
+
+// mongoose.deleteModel("Reservations");
 
 const ReservationModel =
   mongoose.models["Reservations"] ||
